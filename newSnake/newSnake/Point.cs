dea5ec1a3f9bd9d91988526a1f675f4dcdfb3da7
Yarikml <yarik0612199@gmail.com>
2019-08-20 15:28:23 +0400

    Внесены декоративные изменения в код обоих классов

--- newSnake/newSnake/Point.cs
+++ newSnake/newSnake/Point.cs
@@ -11,5 +11,10 @@ namespace newSnake
         public int x;
         public int y;
         public char sym;
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
 }
